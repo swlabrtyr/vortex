22f91e4917ff64c7c0b43ec5e25543ecd07c6357
Thomas Roberson <tyr504@gmail.com>
2019-10-20 10:45:40 +0800

    added title

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,19 @@ import "../styles.css";
 
 const App = () => (
   <div>
-
     <div className="vortex-app">
-
       <div className="controls">
-    <div className="instructions">
-      <p>
-        Input the desired note in the text input
-        <br /> ie "A4" or "C#5"
-        <br />
-        <br /> No flats (b), <strong>only</strong> sharps (#)
-        <br />
-        <br /> Click the number to disable/enable a note
-      </p>
-    </div>
+      <h4><strong>Vortex</strong></h4>
+        <div className="instructions">
+          <p>
+            Input the desired note in the text input
+            <br /> ie "A4" or "C#5"
+            <br />
+            <br /> No flats (b), <strong>only</strong> sharps (#)
+            <br />
+            <br /> Click the number to disable/enable a note
+          </p>
+        </div>
         <PlayPause />
         <AddEvent />
         <RemoveEvent />
