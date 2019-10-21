3b67896b00f6dcabb941fa544c617924aea6d752
Thomas Roberson <tyr504@gmail.com>
2019-10-22 00:14:01 +0800

    simplified instructions

--- src/components/App.js
+++ src/components/App.js
@@ -14,20 +14,21 @@ const App = () => (
           <h4>
             <strong>Vortex</strong>
           </h4>
-          {/* <p>
-            <br /> 1) Input the desired note in the text input
+          <br />
+          <p>
+            <br /> Type a note + octave & press enter
             <br />
-            <br /> 2) Notes <strong>must</strong> include an octave number
+            <br /> Ex. "A4" or "C#5"
             <br />
-            <br /> 3) Ex. "A4" or "C#5"
-            <br />
-            <br /> 4) No flats (b), <strong>only</strong> sharps (#)
-            <br />
-            <br /> 5) Click the number to disable/enable a note.
-            <br />
-            <br /> 6) Notes can be added by simply pressing{" "}
-            <strong>Enter</strong>
-          </p> */}
+            {/* <br /> 3)  */}
+            {/* <br /> */}
+            {/* <br /> 4) No flats (b), <strong>only</strong> sharps (#) */}
+            {/* <br /> */}
+            {/* <br /> 5) Click the number to disable/enable a note. */}
+            {/* <br /> */}
+            {/* <br /> 6) Notes can be added by simply pressing{" "} */}
+            {/* <strong>Enter</strong> */}
+          </p>
         </div>
         <AddEvent />
         <RemoveEvent />
